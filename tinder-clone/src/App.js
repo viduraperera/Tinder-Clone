13bc5a87ad3635aa01f783aba5a500fc5a2d3c82
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-04-07 17:34:44 +0530

    finish header styles

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,10 +1,18 @@
-
+import React from "react";
 import './App.css';
-
+import Header from "./Header";
+import TinderCards from "./TinderCards";
 function App() {
   return (
+      //BEM class naming
     <div className="App">
-      <h1>Lets build the MERN TINDER clone</h1>
+
+        {/* header */}
+        <Header></Header>
+        {/* Tinder Card */}
+        <TinderCards></TinderCards>
+        {/* Swipe Buttons */}
+
     </div>
   );
 }
---
+++ tinder-clone/src/TinderCards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./TinderCard.css"
+function TinderCards(){
+    return (
+        <div className={"tinderCards"}>
+
+        </div>
+    );
+};
+
+export default TinderCards;
